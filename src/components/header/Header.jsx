12f4ed684afa94bec9ecbdd97e5fe7bb11cce6c9
ibrahimali-ca <19438667+ibrahimali-ca@users.noreply.github.com>
2022-06-13 17:38:28 -0400

    added header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      Header
+      <div className="headerList">
+        <div className="headerListItem"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
