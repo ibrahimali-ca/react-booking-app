6eadae64bfd2ae309812ddd0fbac1374a0378526
ibrahimali-ca <19438667+ibrahimali-ca@users.noreply.github.com>
2022-06-13 13:08:10 -0400

    added single hotel page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
       </Routes>
     </BrowserRouter>
   );
